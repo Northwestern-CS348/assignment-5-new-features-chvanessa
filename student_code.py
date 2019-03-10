95f288aa17f6ea6a282ba966c7f83b168ce38367
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2019-03-10 12:19:38 -0500

    done

--- student_code.py
+++ student_code.py
@@ -1,4 +1,8 @@
 import languageTools, utilities, reader, kb, memory, core
+import spacy # added
+print(spacy.__version__)
+
+nlp = spacy.load('en') #added
 
 #Red = "\u001b[31m"
 #Grn = "\u001b[32m"
@@ -34,32 +38,32 @@ def buildFeatureStatement(tree):
     print("Stubbed out version of buildFeatureStatement")
     # As with core.buildExistentials, we want to get out the ROOT and the primary NOUN. But here, the noun will
     # the subject. You call languageTools.extractRoot on the parse tree
-
+    root = languageTools.extractRoot(tree)
  ##### Your code to extractRoot here
 
     # To get the subject of a verb, we use languageTools.extractSubject on the ROOT
-
+    subject = languageTools.extractSubject(root)
 ##### Your code to extractSubject here
 
     # Once we have the primary noun, we then want to resolve it, that is, figure out what it refers
     # to using core.resolveObjectFOPC. Resolve object will give us a list of names that are referred to by the
     # words in the text.  They will always be names of existings objects. You need the names for the
     # other functions
-
+    lon = core.resolveObjectFOPC(subject)
 ##### Your code to resolveObjectFOPC here -- this will build some FOPC and Assert it
 
     # Then we need to figure out what is going to modify it. To do this, we go back to our verb and for
-    # any modifiers associted with it. These will either be prepositional phrases or adjectives.
+    # any modifiers associated with it. These will either be prepositional phrases or adjectives.
 
     # To get the prepositional phrases, we can use core.findAndAttachPrepObjectsFOPC that takes the ROOT and
     # the names and will build any FOPC associated with prepositional objects it finds and assert it.
-
+    core.findAndAttachPrepObjectsFOPC(root, subject, lon)
 ##### Your code to resolveObjectFOPC here -- this takes the root, the primary (suibject) and your names
 
     # Next we pull out any features associated with the object and attach them to the object. We can use
     # core.findAndAssertFeaturesFOPC to do this.  Like core.findAndAttachPrepObjectsFOPC, it takes the ROOT and
     # a list of names and builds the FOPC associated with any adjectives it finds
-
+    core.findAndAssertFeaturesFOPC(root, lon)
 ##### Your code to findAndAssertFeaturesFOPC here -- this takes the root and your names
-
+    core.findAndAssertDefinitionsFOPC(root, lon)
 ##### Your code to findAndAssertDefinitionsFOPC -- this takes the root and your names
